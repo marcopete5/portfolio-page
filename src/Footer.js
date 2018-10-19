15048411d3defffd72a4672a10d0902f648cb4c8
Bob Ziroll <elderziroll@gmail.com>
2018-10-19 11:01:56 -0600

    Testing a PR
    I'm going to delete my Github Fork of this repo before you accept the PR to see if it works

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => {
 
     return (
+        // Consider removing this extra outer div
         <div>
             <div style={{textAlign: "center"}}>
                 <a href="https://github.com/marcopete5"><img id='gitPic' src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" alt=""/></a>
